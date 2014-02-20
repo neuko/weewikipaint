b28c5df68da8f19ce97bd204876a46ccfa81b196
Kobe Neuckermans <kobeneuckermans@gmail.com>
2014-02-20 21:36:08 +0100

    Setup strict lint options

--- Jakefile.js
+++ Jakefile.js
@@ -1,10 +1,38 @@
-// Jakefile is a collection of tests based on jake
+/* global, desc, task, jake, fail, complete */
+(function () {
+    "use strict";
 
-desc("default");
-task("default", ["lint"]);
+    desc("default");
+    task("default", ["lint"]);
 
-desc("lint everything");
-task("lint", [], function () {
-   var lint = require("./build/lint/lint_runner.js");
-    lint.validateFile("Jakefile.js", {},{});
-});
+    desc("lint everything");
+    task("lint", [], function () {
+       var lint = require("./build/lint/lint_runner.js");
+       var files = new jake.FileList();
+        files.include("**/*.js");
+        files.exclude("node_modules");
+        var options = nodeLintOptions();
+        lint.validateFileList(files.toArray(), options, {});
+    });
+
+    function nodeLintOptions () {
+        return {
+            bitwise: true,
+            curly: true,
+            eqeqeq: true,
+            forin: true,
+            immed: true,
+            latedef: true,
+            newcap: true,
+            noarg: true,
+            noempty: true,
+            nonew: true,
+            regexp: true,
+            undef: true,
+            strict: true,
+            trailing: true,
+            node: true
+        };
+    }
+}());
+
